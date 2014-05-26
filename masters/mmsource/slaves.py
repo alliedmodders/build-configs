4255f7484c461f23ee1b1bf4ebd28a72c6c333e4
David Anderson <dvander@alliedmods.net>
2014-05-26 14:55:15 -0700

    Add Metamod:Source configs.

---
+++ masters/mmsource/slaves.py
@@ -0,0 +1,18 @@
+# vim: set ts=4 sw=4 sts=4 tw=99 et ft=python :
+Slaves = {
+    'linux': {
+        'os': 'linux',
+        'compiler': 'clang',
+    },
+    'win32': {
+        'os': 'windows',
+        'flags': {
+            'max_builds': 1,
+        },
+    },
+    'osx10.9': {
+        'os': 'mac',
+        'display': 'mac',
+    },
+}
+
