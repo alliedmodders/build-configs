8e346e9d81234c651868b39ec27bf48a9620bd9e
Nick Hastings <nshastings@gmail.com>
2023-09-29 16:48:25 -0400

    mmsource: Clang 3.8 -> Clang 8.0

--- masters/mmsource/slaves.py
+++ masters/mmsource/slaves.py
@@ -3,6 +3,17 @@ Slaves = {
     'linux': {
         'os': 'linux',
         'compiler': 'clang-3.8',
+        'versions' : [
+            '1.11',
+            '1.12',
+        ]
+    },
+    'linux-2.0': {
+        'os': 'linux',
+        'compiler': 'clang-8',
+        'versions' : [
+            '2.0',
+        ]
     },
     'win32': {
         'os': 'windows',
